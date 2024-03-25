9c748f37225ed9523edb623e66981cd0212429af
Thomas <19950277+CaptThomas@users.noreply.github.com>
2024-03-25 09:53:49 -0400

    working

--- main.py
+++ main.py
@@ -2,31 +2,84 @@ import time
 from selenium import webdriver
 import chromedriver_autoinstaller
 from selenium.webdriver.common.by import By
+
 # Initialize the browser
 chromedriver_autoinstaller.install()
 # Install Chrome web driver from selenium package
 browser = webdriver.Chrome()
 
-#CONFIGURATIONS
+# CONFIGURATIONS
 ### CHANGE ME! ###
 STATE = "Massachusetts"
 CITY = "Winchester"
 ### CHANGE ME! ###
-link = "https://411.info/white-pages/"+STATE+"/"+CITY
+numbers = []
+# Open numbers.csv and write all numbers to numbers list
+with open("numbers.csv") as f:
+    for line in f:
+        numbers.append(line.strip())
+link = "https://411.info/white-pages/" + STATE + "/" + CITY
 browser.get(link)
-for let in ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]:
-    browser.get(link+"?letter="+let)
+for let in [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+]:
+    browser.get(link + "?letter=" + let)
     time.sleep(1)
     # Get all elements in each div class "span4"
     elements = browser.find_elements(By.CLASS_NAME, "span4")
     for element in elements:
         # Get all elements in each div class that are a tags
-        a_elements = element.find_elements(By.TAG_NAME, "a")
+        try:
+            a_elements = element.find_elements(By.TAG_NAME, "a")
+        except:
+            continue
         # New empty list to store links
         links = []
         for a_element in a_elements:
             # Get the href of each a tag and store in links
             links.append(a_element.get_attribute("href"))
-        print(links)
+        for link in links:
+            browser.get(link)
+            time.sleep(1)
+            div_elements = browser.find_elements(By.CLASS_NAME, "phone")
+            for div_element in div_elements:
+                # Append to numbers if not already in the list
+                if div_element.text not in numbers:
+                    numbers.append(div_element.text)
+                    # Write numbers to file
+                    with open("numbers.csv", "a") as f:
+                        f.write("%s\n" % div_element.text)
+
+# Save all numbers to a csv file
+with open("numbers.csv", "w") as f:
+    for number in numbers:
+        f.write("%s\n" % number)
+    f.close()
+
 # Don't forget to close the browser when you're done
 browser.quit()
